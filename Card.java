c67ac313007144302d1dc3ef7161f53b5ba47bea
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-09-13 13:12:06 +0800

    Add Cards

---
+++ Card.java
@@ -0,0 +1,11 @@
+public class Card{
+    private String name;
+    public Card(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+}
