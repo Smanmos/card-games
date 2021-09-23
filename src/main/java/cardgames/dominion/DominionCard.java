f0bc9bef4c934e725a3f4b67b0aa1e2d62f1d534
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-09-24 03:48:26 +0800

    Change DominionCard to concrete class

--- src/main/java/cardgames/dominion/DominionCard.java
+++ src/main/java/cardgames/dominion/DominionCard.java
@@ -5,13 +5,22 @@ import cardgames.dominion.instruction.Instruction;
 
 import java.util.EnumSet;
 
-public abstract class DominionCard extends Card{
+public class DominionCard extends Card{
     private EnumSet<DominionTypes> types;
+    private int cost;
+    private Instruction effect;
 
-    public DominionCard(String name, EnumSet<DominionTypes> types) {
+    public DominionCard(String name, EnumSet<DominionTypes> types, int cost, Instruction effect){
         super(name);
         this.types = EnumSet.copyOf(types);
+        this.cost = cost;
     }
 
-    public abstract Instruction getEffect();
+    public int getCost(){
+        return cost;
+    }
+
+    public Instruction getEffect(){
+        return effect;
+    }
 }
