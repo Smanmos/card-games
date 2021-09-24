847ec593bf231edf29b28ecd422c6b7a5ed1f683
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-09-24 19:22:14 +0800

    Add new constructor for DominionTurn
    The new contructor supplies all attributes of DominionTurn

--- src/main/java/cardgames/dominion/DominionTurn.java
+++ src/main/java/cardgames/dominion/DominionTurn.java
@@ -13,9 +13,37 @@ public class DominionTurn {
         numActions = 1;
         numBuys = 1;
         numCoins = 0;
+        phase = DominionPhase.ACTION_PHASE;
+    }
+
+    public DominionTurn(DominionPlayerState playerState, DominionPhase phase,
+            int numActions, int numBuys, int numCoins){
+        this.playerState = playerState;
+        this.phase = phase;
+        this.numActions = numActions;
+        this.numBuys = numBuys;
+        this.numCoins = numCoins;
     }
 
     public void addCoins(int addedCoins){
         numCoins += addedCoins;
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if (this == other){
+            return true;
+        }
+        else if (!(other instanceof DominionTurn)){
+            return false;
+        }
+        else {
+            DominionTurn otherTurn = (DominionTurn)other;
+            return playerState.equals(otherTurn.playerState)
+                    && phase.equals(otherTurn.phase)
+                    && numActions == otherTurn.numActions
+                    && numBuys == otherTurn.numBuys
+                    && numCoins == otherTurn.numCoins;
+        }
     }
 }
