abb9a125bd1ddcdf76cb04b9d12db8839b64ff6b
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-09-24 19:16:55 +0800

    Implement equals() for DominionPlayerState

--- src/main/java/cardgames/dominion/DominionPlayerState.java
+++ src/main/java/cardgames/dominion/DominionPlayerState.java
@@ -4,6 +4,8 @@ import cardgames.Deck;
 import cardgames.DiscardPile;
 import cardgames.Hand;
 
+import java.util.ArrayList;
+
 public class DominionPlayerState {
     private Deck<DominionCard> deck;
     private Hand<DominionCard> hand;
@@ -11,7 +13,36 @@ public class DominionPlayerState {
     private PlayArea playArea;
     private DominionPhase phase;
 
+    public DominionPlayerState(Deck<DominionCard> deck, Hand<DominionCard> hand,
+            DiscardPile<DominionCard> discardPile, PlayArea playArea,
+            DominionPhase phase){
+        this.deck = deck;
+        this.hand = hand;
+        this.discardPile = discardPile;
+        this.playArea = playArea;
+        this.phase = phase;
+    }
+
     public DominionPlayerState(){
-        deck = DominionUtil.STARTING_DECK;
+        this(new Deck<>(new ArrayList<>()), new Hand<>(), new DiscardPile<>(), new PlayArea(),
+                DominionPhase.ACTION_PHASE);
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if (this == other){
+            return true;
+        }
+        else if (!(other instanceof DominionPlayerState)){
+            return false;
+        }
+        else {
+            DominionPlayerState otherState = (DominionPlayerState)other;
+            return deck.equals(otherState.deck)
+                    && hand.equals(otherState.hand)
+                    && discardPile.equals(otherState.discardPile)
+                    && playArea.equals(otherState.playArea)
+                    && phase.equals(otherState.phase);
+        }
     }
 }
