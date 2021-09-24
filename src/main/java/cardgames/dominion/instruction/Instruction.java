d8d7a944a39065434d88371536505b40434cd46b
kawaiigabrielneko <57220480+kawaiigabrielneko@users.noreply.github.com>
2021-09-24 15:55:50 +0800

    Add method to create Instruction from JSONArray

--- src/main/java/cardgames/dominion/instruction/Instruction.java
+++ src/main/java/cardgames/dominion/instruction/Instruction.java
@@ -1,7 +1,17 @@
 package cardgames.dominion.instruction;
 
 import cardgames.dominion.DominionTurn;
+import org.json.JSONArray;
 
 public abstract class Instruction {
     public abstract void execute(DominionTurn turn);
+
+    public static Instruction of(JSONArray jsonInstruction){
+        switch (jsonInstruction.getString(0)){
+        case "add_coin":
+            return new InstructionAddCoin(jsonInstruction.getInt(1));
+        default:
+            throw new IllegalArgumentException("Invalid instruction");
+        }
+    }
 }
